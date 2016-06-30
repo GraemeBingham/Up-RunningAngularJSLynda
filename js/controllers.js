a050498b802a372958f48857bf49b387cb77defd
GraemeBingham <binghamgraeme@gmail.com>
2016-07-01 08:27:28 +1000

    Added JSON Data and Controller to import it

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MyController', function MyController($scope) {
-  $scope.author = {
-    'name' : 'Ray Villalobos',
-    'title' : 'Staff Author',
-    'company': 'Lynda.com'
-  }
-});
+myApp.controller('MyController', ['$scope', '$http', function ($scope, $http) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data;
+  });
+}]);
